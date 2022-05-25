df24c68ae2b32b5f7a634e3f5e341bfdbad5542b
Dominik Conway <dominik.conway@gmail.com>
2022-05-24 20:51:06 -0700

    adds initial backend language

---
+++ server.js
@@ -0,0 +1,42 @@
+///////////////////////////////
+// DEPENDENCIES
+////////////////////////////////
+
+// get .env variables
+require("dotenv").config()
+
+// pull PORT from .env, give default value of 3001
+// pull DATABASE_URL from .env
+const { PORT = 3001, DATABASE_URL } = process.env
+
+// import express
+const express = require("express")
+// create application object
+const app = express()
+// import mongoose
+const mongoose = require("mongoose")
+
+///////////////////////////////
+// DATABASE CONNECTION
+////////////////////////////////
+
+// Establish Connection
+mongoose.connect(DATABASE_URL)
+// Connection Events
+mongoose.connection
+  .on("open", () => console.log("You are connected to MongoDB"))
+  .on("close", () => console.log("You are disconnected from MongoDB"))
+  .on("error", (error) => console.log(error))
+
+///////////////////////////////
+// ROUTES
+////////////////////////////////
+// create a test route
+app.get("/", (req, res) => {
+  res.send("hello world")
+})
+
+///////////////////////////////
+// LISTENER
+////////////////////////////////
+app.listen(PORT, () => console.log(`listening on PORT ${PORT}`))
